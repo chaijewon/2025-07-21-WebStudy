bf8260e2ee68790a9bf4d767cd9b042e64f8bdd6
chaijewon <vcandjava@nate.com>
2025-10-13 16:10:55 +0900

    1111

---
+++ JSPBasicProject_2/src/main/java/com/sist/dao/EmpDAO.java
@@ -0,0 +1,34 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+/*
+ *   2 => ibatis => opensource 
+ *   3 => mybatis => google에 인수 
+ */
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.*;
+import com.sist.vo.*;
+public class EmpDAO {
+   private static SqlSessionFactory ssf;
+   static
+   {
+	   // XML에 등록된 데이터 읽기 => XML(파싱)
+	   try
+	   {
+		   Reader reader=Resources.getResourceAsReader("Config.xml");
+		   ssf=new SqlSessionFactoryBuilder().build(reader);
+	   }catch(Exception ex) {}
+   }
+   // 기능 설정 
+   public static List<EmpVO> empListData()
+   {
+	   SqlSession session=ssf.openSession();// getConnection()
+	   List<EmpVO> list=session.selectList("empListData");
+	   session.close();// 반환 => disConnection()
+	   return list;
+   }
+}
