5849bca557660b3d65e812e9bd8522c11f7665f3
chaijewon <vcandjava@nate.com>
2025-10-15 13:41:19 +0900

    1111

---
+++ JSPBasicProject_4/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,15 @@
+package com.sist.dao;
+/*
+ *   
+ *   1. 목록 ------ 
+ *                | sendRedirect
+ *   2. 글쓰기 -----
+ *   3. 상세보기 ---
+ *               |
+ *   4. 수정 ------
+ *   5. 삭제 ----------- 목록
+ *      
+ */
+public class BoardDAO {
+
+}
