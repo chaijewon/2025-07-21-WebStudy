3b36a50b515547cde96d15266f6576f2138cd7c2
chaijewon <vcandjava@nate.com>
2025-10-15 14:47:47 +0900

    111

--- JSPBasicProject_4/src/main/java/com/sist/dao/BoardDAO.java
+++ JSPBasicProject_4/src/main/java/com/sist/dao/BoardDAO.java
@@ -10,6 +10,91 @@ package com.sist.dao;
  *   5. 삭제 ----------- 목록
  *      
  */
-public class BoardDAO {
+import java.util.*;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.sist.vo.BoardVO;
+
+import java.io.*;
+public class BoardDAO {
+   private static SqlSessionFactory ssf;
+   static
+   {
+	   // XML을 파싱 => 저장된 데이터 읽기 
+	   try
+	   {
+		   // XML 파일 읽기 
+		   Reader reader=Resources.getResourceAsReader("Config.xml");
+		   ssf=new SqlSessionFactoryBuilder().build(reader);
+		   // map에 저장 => key=id value=SQL
+	   }catch(Exception ex) 
+	   {
+		   ex.printStackTrace();
+	   }
+   }
+   // 목록 출력 
+   /*
+    *   <select id="boardListData" resultType="BoardVO" 
+		  parameterType="hashmap">
+		   SELECT no,subject,name,
+		          TO_CHAR(regdate,'YYYY-MM-DD') as dbday, 
+		          hit,num
+		   FROM (SELECT no,subject,name,regdate,hit,rownum as num
+		   FROM (SELECT no,subject,name,regdate,hit 
+		   FROM jspBoard ORDER BY no DESC))
+		   WHERE num BETWEEN #{start} AND #{end}
+		 </select>
+    */
+   public static List<BoardVO> boardListData(Map map)
+   {
+	   SqlSession session=ssf.openSession();
+	   // getConnection()
+	   List<BoardVO> list=
+			  session.selectList("boardListData",map);
+	   session.close();
+	   return list;
+   }
+   /*
+    *   <select id="boardTotalPage" resultType="int">
+		   SELECT CEIL(COUNT(*)/10.0) FROM jspBoard
+		 </select>
+    */
+   public static int boardTotalPage()
+   {
+	   SqlSession session=ssf.openSession();
+	   int total=session.selectOne("boardTotalPage");
+	   session.close();
+	   return total;
+   }
+   /*
+    *   <insert id="boardInsert" parameterType="BoardVO">
+		   <selectKey keyProperty="no" resultType="int" 
+		    order="BEFORE"
+		   >
+		     SELECT NVL(MAX(no)+1,1) as no FROM jspBoard
+		   </selectKey>
+		   INSERT INTO jspBoard(no,name,subject,content,pwd)
+		   VALUES(#{no},#{name},#{subject},#{content},#{pwd})
+	   </insert>
+    */
+   public static void boardInsert(BoardVO vo)
+   {
+	   SqlSession session=ssf.openSession(true);
+	   session.insert("boardInsert",vo);
+	   //session.commit();
+	   session.close();
+   }
 }
+
+
+
+
+
+
+
+
+
