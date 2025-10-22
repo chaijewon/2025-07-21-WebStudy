947b7a0e49fe4140731af89bb08bb72026163e87
chaijewon <vcandjava@nate.com>
2025-10-22 10:57:04 +0900

    1111

--- JSPFrontProject_4/src/main/java/com/sist/controller/FileConfig.java
+++ JSPFrontProject_4/src/main/java/com/sist/controller/FileConfig.java
@@ -1,5 +1,33 @@
 package com.sist.controller;
-
+import java.io.*;
+import java.util.*;
 public class FileConfig {
-
+   public static void main(String[] args) {
+	   packageClassData("C:\\webDev\\webStudy\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\JSPFrontProject_4\\WEB-INF\\classes", 
+			   "com.sist.model");
+   }
+   public static List<String> packageClassData(String path,
+		        String pack)
+   {
+	   List<String> list=new ArrayList<String>();
+	   try
+	   {
+		   String strPack=pack.replace(".", File.separator);
+		   String strPath=path+File.separator+strPack;
+		   System.out.println(strPath);
+		   File dir=new File(strPath);
+		   File[] listFiles=dir.listFiles();
+		   for(File f:listFiles)
+		   {
+			   //System.out.println(f.getName());
+			   String name=f.getName();
+			   String ext=name.substring(name.lastIndexOf(".")+1);
+			   if(ext.equals("class") && !name.equals("Model"))
+			   {
+				   
+			   }
+		   }
+	   }catch(Exception ex){}
+	   return list;
+   }
 }
